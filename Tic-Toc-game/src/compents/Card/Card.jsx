7f1a1574b476de40e736563e86b1a6aad0434077
Mayanks08 <95845450+Mayanks08@users.noreply.github.com>
2024-02-07 13:15:05 +0530

    Tic Toc game

---
+++ Tic-Toc-game/src/compents/Card/Card.jsx
@@ -0,0 +1,16 @@
+import Icon from "../Icon/Icon";
+import './Card.css'
+function Card({player, onPlay,index,gameEnd} ){
+    let icon= <Icon/>
+    if(player == 'x') {
+        icon = <Icon name ="cross" />
+    }else if(player =='o'){
+        icon = <Icon name="circle" />
+    }
+    return(
+        <div className="card" onClick={() => !gameEnd && player =="" && onPlay(index)}>
+          {icon}
+        </div>
+    )
+}
+export default Card;
